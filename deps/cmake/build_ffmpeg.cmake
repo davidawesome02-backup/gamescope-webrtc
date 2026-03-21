23f6946698ecbabff04a183abc450c0fed387477
davidawesome02-backup <138835606+davidawesome02-backup@users.noreply.github.com>
2026-03-20 22:45:09 -0700

    Fixish build process, make binary smaller hopefully and faster to build.

--- deps/cmake/build_ffmpeg.cmake
+++ deps/cmake/build_ffmpeg.cmake
@@ -10,7 +10,6 @@ set(FFMPEG_LIBS
     ${FFMPEG_LIB_DIR}/libavcodec.a
     ${FFMPEG_LIB_DIR}/libavformat.a
     ${FFMPEG_LIB_DIR}/libavutil.a
-    ${FFMPEG_LIB_DIR}/libswscale.a
 )
 
 ExternalProject_Add(ffmpeg_build
@@ -26,65 +25,45 @@ ExternalProject_Add(ffmpeg_build
         --enable-static
         --enable-pic
         --enable-lto
+        --enable-gpl
 
         # reduce size
         --disable-programs
         --disable-doc
         --disable-debug
         --disable-autodetect
-
-        # remove everything
+        --enable-small             # reduces size without breaking helpers
         --disable-everything
 
         # core libs
         --enable-avcodec
         --enable-avformat
         --enable-avutil
-        --enable-swscale
 
         # threading
         --enable-pthreads
 
         # fast CPU optimizations (cant use asm because of PIC)
         --enable-runtime-cpudetect
-
         # unfortunately required to make ffmpeg compile into a single .a file :(
         --disable-asm
 
         # H264 support
-        # --enable-decoder=h264
-        # --enable-parser=h264
-        # --enable-encoder=libx264
-        # --enable-encoder=libx264rgb
-        --enable-encoder=h264
-        --enable-encoder=h264_vaapi
 
         --enable-hwaccel=h264_vulkan
         --enable-encoder=h264_vulkan
 
-        # --enable-libx264
-        --enable-vulkan # not real?
+        --enable-vulkan
         # --enable-vulkan-static
-        # --enable-vaapi
-        --enable-gpl
-
 
         # containers
-        --enable-muxer=flv
         --enable-muxer=rtp
+
+
+        # to remove
         --enable-muxer=rtp_mpegts
-        # --enable-demuxer=flv
-
-        # streaming
-        --enable-network
-        --enable-protocol=rtmp
-
-        # scaling
         --enable-swscale
-
-        --enable-parser=mpegvideo  # parser framework dependency
-        --enable-avutil            # ensure libavutil builds
-        --enable-small             # reduces size without breaking helpers
+        --enable-parser=mpegvideo
 
     BUILD_COMMAND make -j
     INSTALL_COMMAND make install
@@ -101,7 +80,6 @@ ExternalProject_Add(ffmpeg_build
 add_library(ffmpeg_avcodec STATIC IMPORTED GLOBAL)
 add_library(ffmpeg_avformat STATIC IMPORTED GLOBAL)
 add_library(ffmpeg_avutil STATIC IMPORTED GLOBAL)
-add_library(ffmpeg_swscale STATIC IMPORTED GLOBAL)
 
 set_target_properties(ffmpeg_avcodec PROPERTIES
     IMPORTED_LOCATION ${FFMPEG_INSTALL}/lib/libavcodec.a)
@@ -112,13 +90,9 @@ set_target_properties(ffmpeg_avformat PROPERTIES
 set_target_properties(ffmpeg_avutil PROPERTIES
     IMPORTED_LOCATION ${FFMPEG_INSTALL}/lib/libavutil.a)
 
-set_target_properties(ffmpeg_swscale PROPERTIES
-    IMPORTED_LOCATION ${FFMPEG_INSTALL}/lib/libswscale.a)
-
 add_dependencies(ffmpeg_avcodec ffmpeg_build)
 add_dependencies(ffmpeg_avformat ffmpeg_build)
 add_dependencies(ffmpeg_avutil ffmpeg_build)
-add_dependencies(ffmpeg_swscale ffmpeg_build)
 
 # interface target for easy linking
 
@@ -132,7 +106,6 @@ target_link_libraries(ffmpeg INTERFACE
     ffmpeg_avcodec
     ffmpeg_avformat
     ffmpeg_avutil
-    ffmpeg_swscale
     pthread
     m
     z
